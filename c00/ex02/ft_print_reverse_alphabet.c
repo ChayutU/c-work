7f0210b597dbd16c66baae2e0d639635341429ed
Chayut <chayut@yellotalk.co>
2022-10-09 16:39:30 +0700

    Add ex02

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chupatha <chupatha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/09 16:38:05 by chupatha          #+#    #+#             */
+/*   Updated: 2022/10/09 16:39:19 by chupatha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_print_reverse_alphabet(void)
+{
+    char i = 'z';
+    while (i >= 'a')
+    {
+        ft_putchar(i);
+        i--;
+    }
+}
