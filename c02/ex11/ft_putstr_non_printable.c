9c0fc3ee16659559103b8a609b560eb8b7f6a82d
Chayut <chayut@yellotalk.co>
2022-10-12 18:44:58 +0700

    Add c02 ex11

---
+++ c02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chupatha <chupatha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/10 21:56:07 by chupatha          #+#    #+#             */
+/*   Updated: 2022/10/12 18:44:39 by chupatha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_is_printable(char c)
+{
+	if (c < ' ' || c > '~')
+	{
+		return (0);
+	}
+	return (1);
+}
+
+void	ft_put_hex(int dec)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	if (dec > 16)
+	{
+		ft_put_hex(dec / 16);
+		ft_put_hex(dec % 16);
+	}
+	else
+		ft_putchar(hex[dec]);
+}
+
+void	ft_print_hex(int dec)
+{
+	ft_putchar('\\');
+	if (dec < 0)
+		dec = dec * -1;
+	if (dec < 16)
+		ft_putchar('0');
+	ft_put_hex(dec);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (ft_is_printable(str[i]))
+		{
+			ft_putchar(str[i]);
+		}
+		else
+		{
+			ft_print_hex(str[i]);
+		}
+		i++;
+	}
+}
